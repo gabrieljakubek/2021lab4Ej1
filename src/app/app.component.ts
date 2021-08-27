2f8e76b5862396ea975ccf1b21c02004ec6910ff
gabrieljakubek <[git gabrieljakubek@gmail.com>
2021-08-26 21:02:25 -0300

    Prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Usuario } from './classes/usuario';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto1';
+  public edadUno!: number;
+  public edadDos!: number;
+  public resultado!: number;
+  public promedio!: number;
+  constructor() {
+    this.edadUno =0;
+    this.edadDos =0;
+    this.resultado = 0;
+    this.promedio = 0;
+  }
+  calcular() {
+    this.resultado = this.edadUno + this.edadDos;
+    this.promedio = this.resultado / 2;
+  }
+  limpiar(){
+    this.edadUno =0;
+    this.edadDos =0;
+    this.resultado = 0;
+    this.promedio = 0;
+  }
 }
+
+
+  
